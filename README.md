cfd722ad0dad88e3d0ee688896e8bdce75362061
Kevin C <Kache@users.noreply.github.com>
2023-09-20 15:37:43 -0700

    Init source

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Rerequests
 
+Drop-in replacement for `requests` with default timeouts and retry support
+
+ * `requests` has no timeout by default and can hang unnecessarily
+   https://github.com/psf/requests/issues/3070
+ * `requests` has poor retry support. Transient errors are not retried by
+   default, and retry functionality is buried under "Lower-Lower-Level Classes"
+   https://github.com/psf/requests/blob/v2.31.0/docs/api.rst?plain=1#L61-L71
+   https://requests.readthedocs.io/en/latest/api/#requests.adapters.HTTPAdapter
+
 [![PyPI - Version](https://img.shields.io/pypi/v/rerequests.svg)](https://pypi.org/project/rerequests)
 [![PyPI - Python Version](https://img.shields.io/pypi/pyversions/rerequests.svg)](https://pypi.org/project/rerequests)
 
@@ -8,6 +17,7 @@
 **Table of Contents**
 
 - [Installation](#installation)
+- [Notes](#notes)
 - [License](#license)
 
 ## Installation
@@ -15,6 +25,40 @@
 ```console
 pip install rerequests
 ```
+
+## Notes
+
+`rerequests` only tweaks "convenience" usages (e.g. requests.{get,put,...}):
+
+ * Timeout of 60 seconds (6 sec connect + 54 sec read), by default
+ * Conveniently configurable `max_retries` and `Retry()`
+ * Retry implements truncated exponential backoff with jitter, by default
+
+And provides the following extensions:
+
+ * Convenient requests.http_raise hook for 4XX and 5XX exceptions
+
+Examples:
+
+```python
+import rerequests as requests  # drop-in replacement
+
+# same usage, but with timeouts (and no retries)
+requests.get(URL)
+requests.get(URL, timeout=(3.05, 27))  # override default
+
+# accessible retry config
+requests.post(URL, max_retries=2)  # retries transient errors despite non-idempotent POST
+requests.put(URL, max_retries=requests.Retry(5, **more_config))
+
+# inline response.raise_for_status() hook, with logging
+requests.delete(URL, hooks=requests.http_raise)
+```
+
+Resources:
+ * https://en.wikipedia.org/wiki/Exponential_backoff#Truncated_exponential_backoff
+ * https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+ * https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/
 
 ## License
 
--- src/rerequests/__init__.py
+++ src/rerequests/__init__.py
@@ -1,3 +1,289 @@
 # SPDX-FileCopyrightText: 2023-present Kevin C <Kache@users.noreply.github.com>
 #
 # SPDX-License-Identifier: MIT
+from __future__ import annotations
+
+import itertools as it
+import logging
+import random
+import typing
+
+import packaging.version as version
+import requests
+import urllib3.util
+from requests import *  # type: ignore in order to be a true drop-in replacement
+
+if version.parse(requests.__version__) >= version.parse('3.0.0'):
+    raise ImportError('rerequests is a drop-in replacement for requests 2, re-evaluate for v3')
+
+if typing.TYPE_CHECKING:
+    import sys
+    if sys.version_info >= (3, 11):
+        from typing import Self
+    else:
+        from typing_extensions import Self
+
+
+DEFAULT_TIMEOUT = (6.1, 54)  # (connect, read), see: https://docs.python-requests.org/en/latest/user/advanced/#timeouts
+TRANSIENT_ERRORS = frozenset({
+    codes.request_timeout,        # 408
+    codes.too_many_requests,      # 429 - may have Retry-After header
+    codes.internal_server_error,  # 500
+    codes.bad_gateway,            # 502
+    codes.service_unavailable,    # 503 - may have Retry-After header
+    codes.gateway_timeout,        # 504
+})
+IDEMPOTENT_METHODS = urllib3.util.Retry.DEFAULT_ALLOWED_METHODS
+log = logging.getLogger(__name__)
+
+
+class Retry(urllib3.util.Retry):
+    """Retry configuration, with nicer defaults and jitter
+
+    see: https://urllib3.readthedocs.io/en/latest/reference/urllib3.util.html#urllib3.util.Retry
+    """
+
+    DEFAULT: Self = None  # type: ignore and assign later
+
+    def __init__(
+        self,
+        total=3,                            # num retries, i.e. 4 requests in total
+        status_forcelist=TRANSIENT_ERRORS,
+        allowed_methods=None,               # retry all methods when instantiating manually/explicitly
+        backoff_factor=1,                   # retry after 1, 2, 4, 8, ...
+        **kwargs,
+    ):
+        super().__init__(
+            total=total,
+            status_forcelist=status_forcelist,
+            allowed_methods=allowed_methods,
+            backoff_factor=backoff_factor,
+            **kwargs,
+        )
+
+    def get_backoff_time(self):
+        # We want to consider only the last consecutive errors sequence (Ignore redirects).
+        consecutive_errors_len = len(
+            list(
+                it.takewhile(lambda x: x.redirect_location is None, reversed(self.history))
+            )
+        )
+        if consecutive_errors_len < 1:  # NOTE: differs from super().get_backoff_time()
+            return 0.0
+
+        backoff_value = self.backoff_factor * (2 ** (consecutive_errors_len - 1))
+        # NOTE: below differs from super().get_backoff_time()
+        jittered = random.uniform(0.0, backoff_value * 2.0)  # "Full Jitter"
+        return min(self.DEFAULT_BACKOFF_MAX, jittered)
+
+    # TODO: configurable upper bound, don't obey when server tells you to sleep for 1000 years
+    # def sleep_for_retry(self, response):
+    #     return super().sleep_for_retry(response)
+
+    def __eq__(self, other):
+        return isinstance(other, type(self)) and vars(other) == vars(self)
+
+    @classmethod
+    def _from_arg(cls, max_retries, **init_kwargs):
+        return max_retries if isinstance(max_retries, cls) else cls(max_retries or 0, **init_kwargs)
+
+
+Retry.DEFAULT = Retry(allowed_methods=IDEMPOTENT_METHODS)  # symmetrical to urllib3.util.retry.DEFAULT
+
+
+def raise_for_status(response: Response, **kwargs):
+    """Response hook for `response.raise_for_status()` and logging
+
+    Example:
+
+    >>> requests.get(URL, hooks={'response': raise_for_status})
+    >>> requests.get(URL, hooks=requests.http_raise)  # alternative
+    """
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        try:
+            log.error(response.json())
+        except Exception:
+            log.error(response.text)
+        raise
+    return response
+
+
+http_raise = {'response': [raise_for_status]}
+
+
+class _ReSession(sessions.Session):
+    """Still WIP, implementation may change!"""
+
+    def __init__(self, *, timeout=DEFAULT_TIMEOUT, max_retries: Retry | int | None = Retry.DEFAULT):
+        super().__init__()
+
+        self.timeout = timeout
+        retry = Retry._from_arg(max_retries, allowed_methods=IDEMPOTENT_METHODS)
+        adapter = sessions.HTTPAdapter(max_retries=retry)
+
+        self.mount('https://', adapter)
+        self.mount('http://', adapter)
+
+    def request(self, method, url, **kwargs):
+        timeout = kwargs.pop('timeout', self.timeout)
+        return super().request(method, url, timeout=timeout, **kwargs)
+
+    @classmethod
+    def _for_convenience_api(cls, kwargs_for_request):
+        max_retries = kwargs_for_request.pop('max_retries', None)
+        return cls(max_retries=Retry._from_arg(max_retries))
+
+
+########
+# Below this point, source is identical to requests.api, save for one usage of _ReSession
+########
+
+
+def request(method, url, **kwargs):
+    """Constructs and sends a :class:`Request <Request>`.
+
+    :param method: method for the new :class:`Request` object: ``GET``, ``OPTIONS``, ``HEAD``, ``POST``, ``PUT``, ``PATCH``, or ``DELETE``.
+    :param url: URL for the new :class:`Request` object.
+    :param params: (optional) Dictionary, list of tuples or bytes to send
+        in the query string for the :class:`Request`.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) A JSON serializable Python object to send in the body of the :class:`Request`.
+    :param headers: (optional) Dictionary of HTTP Headers to send with the :class:`Request`.
+    :param cookies: (optional) Dict or CookieJar object to send with the :class:`Request`.
+    :param files: (optional) Dictionary of ``'name': file-like-objects`` (or ``{'name': file-tuple}``) for multipart encoding upload.
+        ``file-tuple`` can be a 2-tuple ``('filename', fileobj)``, 3-tuple ``('filename', fileobj, 'content_type')``
+        or a 4-tuple ``('filename', fileobj, 'content_type', custom_headers)``, where ``'content-type'`` is a string
+        defining the content type of the given file and ``custom_headers`` a dict-like object containing additional headers
+        to add for the file.
+    :param auth: (optional) Auth tuple to enable Basic/Digest/Custom HTTP Auth.
+    :param timeout: (optional) How many seconds to wait for the server to send data
+        before giving up, as a float, or a :ref:`(connect timeout, read
+        timeout) <timeouts>` tuple.
+    :type timeout: float or tuple
+    :param allow_redirects: (optional) Boolean. Enable/disable GET/OPTIONS/POST/PUT/PATCH/DELETE/HEAD redirection. Defaults to ``True``.
+    :type allow_redirects: bool
+    :param proxies: (optional) Dictionary mapping protocol to the URL of the proxy.
+    :param verify: (optional) Either a boolean, in which case it controls whether we verify
+            the server's TLS certificate, or a string, in which case it must be a path
+            to a CA bundle to use. Defaults to ``True``.
+    :param stream: (optional) if ``False``, the response content will be immediately downloaded.
+    :param cert: (optional) if String, path to ssl client cert file (.pem). If Tuple, ('cert', 'key') pair.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+
+    Usage::
+
+      >>> import requests
+      >>> req = requests.request('GET', 'https://httpbin.org/get')
+      >>> req
+      <Response [200]>
+    """
+
+    # By using the 'with' statement we are sure the session is closed, thus we
+    # avoid leaving sockets open which can trigger a ResourceWarning in some
+    # cases, and look like a memory leak in others.
+    with _ReSession._for_convenience_api(kwargs) as session:
+        return session.request(method=method, url=url, **kwargs)
+
+
+def get(url, params=None, **kwargs):
+    r"""Sends a GET request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param params: (optional) Dictionary, list of tuples or bytes to send
+        in the query string for the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    kwargs.setdefault('allow_redirects', True)
+    return request('get', url, params=params, **kwargs)
+
+
+def options(url, **kwargs):
+    r"""Sends an OPTIONS request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    kwargs.setdefault('allow_redirects', True)
+    return request('options', url, **kwargs)
+
+
+def head(url, **kwargs):
+    r"""Sends a HEAD request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param \*\*kwargs: Optional arguments that ``request`` takes. If
+        `allow_redirects` is not provided, it will be set to `False` (as
+        opposed to the default :meth:`request` behavior).
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    kwargs.setdefault('allow_redirects', False)
+    return request('head', url, **kwargs)
+
+
+def post(url, data=None, json=None, **kwargs):
+    r"""Sends a POST request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    return request('post', url, data=data, json=json, **kwargs)
+
+
+def put(url, data=None, **kwargs):
+    r"""Sends a PUT request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    return request('put', url, data=data, **kwargs)
+
+
+def patch(url, data=None, **kwargs):
+    r"""Sends a PATCH request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    return request('patch', url, data=data, **kwargs)
+
+
+def delete(url, **kwargs):
+    r"""Sends a DELETE request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    return request('delete', url, **kwargs)
